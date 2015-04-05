43b1d9a1e377fe36f5ea8e7f288a8d78cc8ecedb
Nobuhiro Ito <iseebi@iseteki.net>
2015-04-05 18:24:50 +0900

    Step2. 検索用のプロパティを作る

--- YouTubeSearch.Core/ViewModels/FirstViewModel.cs
+++ YouTubeSearch.Core/ViewModels/FirstViewModel.cs
@@ -1,15 +1,31 @@
 using Cirrious.MvvmCross.ViewModels;
+using Cirrious.CrossCore;
 
 namespace YouTubeSearch.Core.ViewModels
 {
     public class FirstViewModel 
 		: MvxViewModel
     {
-		private string _hello = "Hello MvvmCross";
-        public string Hello
-		{ 
-			get { return _hello; }
-			set { _hello = value; RaisePropertyChanged(() => Hello); }
-		}
+        public string SearchWord
+        {
+            get { return _searchWord; }
+            set
+            {
+                _searchWord = value;
+                RaisePropertyChanged(() => SearchWord);
+            }
+        }
+        string _searchWord;
+
+        public MvxCommand DoSearchCommand
+        {
+            get
+            {
+                return _doSearchCommand ?? (_doSearchCommand = new MvxCommand(() => {
+                    Mvx.Trace("SearchTapped: {0}", SearchWord);
+                }));
+            }
+        }
+        MvxCommand _doSearchCommand;
     }
 }
