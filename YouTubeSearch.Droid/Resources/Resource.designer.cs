4ec57f2fd3151ee824ae18891567643cdcdcc2ea
Nobuhiro Ito <iseebi@iseteki.net>
2015-04-07 06:34:26 +0900

    Step 6-2: MvxImageView を使うために File プラグインと DownloadCache プラグインを追加

--- YouTubeSearch.Droid/Resources/Resource.designer.cs
+++ YouTubeSearch.Droid/Resources/Resource.designer.cs
@@ -102,7 +102,10 @@ namespace YouTubeSearch.Droid
 			public const int FirstView = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int SplashScreen = 2130903041;
+			public const int item_search_result = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int SplashScreen = 2130903042;
 			
 			static Layout()
 			{
