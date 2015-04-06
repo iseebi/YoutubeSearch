0db5b7d6752aeaab8e320aa8b03a8001b49b6ed9
Nobuhiro Ito <iseebi@iseteki.net>
2015-04-07 07:25:51 +0900

    Step 8-1: WebBrowser プラグインを追加

---
+++ YouTubeSearch.Touch/Bootstrap/WebBrowserPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace YouTubeSearch.Touch.Bootstrap
+{
+    public class WebBrowserPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<Cirrious.MvvmCross.Plugins.WebBrowser.PluginLoader, Cirrious.MvvmCross.Plugins.WebBrowser.Touch.Plugin>
+    {
+    }
+}
